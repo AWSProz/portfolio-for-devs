30987992f07437454fca5348dde47425b3685c21
Steffany Sympson <ssy.marques@gmail.com>
2023-11-12 19:05:06 -0300

    NewPages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -260,3 +255,11 @@
 			});
 
 })(jQuery);
+
+var iframeShowing = false;
+
+function show() {
+    var iframe1 = document.getElementById('iframe');
+    iframe1.style.display = iframeShowing ? 'block' : 'none';
+    iframeShowing = !iframeShowing;
+}
